1280c31220e7e9c05eb5756cf21bd44e161bf0a6
Robert <rlancer@arachno.com>
2016-09-03 18:00:54 -0400

    replace container

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,6 +1,6 @@
-import React, { Component, PropTypes } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React, {Component, PropTypes} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
 import Header from '../components/Header';
 import MainSection from '../components/MainSection';
 import * as TodoActions from '../actions/todos';
@@ -22,12 +22,13 @@ export default class App extends Component {
   };
 
   render() {
-    const { todos, actions } = this.props;
+    const {todos, actions} = this.props;
 
     return (
-      <div className={style.normal}>
-        <Header addTodo={actions.addTodo} />
-        <MainSection todos={todos} actions={actions} />
+      <div>
+        {/*<Header addTodo={actions.addTodo} />
+         <MainSection todos={todos} actions={actions} />*/}
+        GET QOUTES AND STUFF HERE
       </div>
     );
   }
