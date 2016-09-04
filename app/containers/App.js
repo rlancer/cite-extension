c7007ef86c967f84844263b5580e3e1c2cf91ac7
Robert <rlancer@arachno.com>
2016-09-03 23:26:23 -0400

    logs the current tab

--- app/containers/App.js
+++ app/containers/App.js
@@ -16,19 +16,54 @@ import style from './App.css';
 )
 export default class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {x: 1};
+  }
+
   static propTypes = {
     todos: PropTypes.array.isRequired,
     actions: PropTypes.object.isRequired
+  };
+
+  log(object) {
+    console.log('log', object);
+    chrome.extension.getBackgroundPage().console.log(object);
+  }
+
+
+  doStuff = ()=> {
+    try {
+      this.setState(oldState=> ({x: oldState.x + 1}));
+      this.log('DO STUFFxxuxx', chrome.tabs);
+
+      chrome.tabs.query({currentWindow: true, active: true}, (tabs)=> {
+        this.log(tabs[0].url);
+      });
+
+      chrome.tabs.getCurrent(tab=> {
+        this.log('tab', tab);
+        this.setState({tab: JSON.stringify(tab)});
+      });
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   render() {
     const {todos, actions} = this.props;
 
     return (
-      <div>
+      <div style={{backgroundColor: 'red', height: '20rem'}}>
+
+        <div style={{padding: '2rem', backgroundColor: 'pink', cursor: 'pointer', color: 'white'}}
+             onClick={this.doStuff}>DO STUFF
+        </div>
+        <div>{this.state.x}</div>
         {/*<Header addTodo={actions.addTodo} />
          <MainSection todos={todos} actions={actions} />*/}
-        GET QOUTES AND STUFF HERE
+        GET QOUTES AND STUFF HEREjjjj;;;;;
+        {this.state.tab}
       </div>
     );
   }
